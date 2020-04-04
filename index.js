a86a425ab51e3a5185bfe5dfc4a92856bf5bb738
Tanner <t.langan.07@gmail.com>
2020-04-04 16:15:00 -0700

    Adding index.html, .css, .js to repo.  These files are currently empty and waiting to be updated

---
+++ index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8"></meta>
+  <title>Etch-A-Sketch</title>
+  <meta name="author" content="Tanner Langan"></meta>
+  <meta name="description" content="Etch-A-Sketch Project for the The Odin Project"></meta>
+  <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+
+  <link href="css/normalize.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <p>Hello, world!</p>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
