12633be57f7c5927b3aeea2911dd014003177895
Tanner <t.langan.07@gmail.com>
2020-04-04 19:28:17 -0700

    Implemented DOM manipulation to add 16x16 divs using for loop and append them to the main grid container

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html>
+const gridContainer = document.querySelector('#gridContainer');
 
-<head>
-  <meta charset="utf-8"></meta>
-  <title>Etch-A-Sketch</title>
-  <meta name="author" content="Tanner Langan"></meta>
-  <meta name="description" content="Etch-A-Sketch Project for the The Odin Project"></meta>
-  <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+for (let index = 0; index < 16*16; index++) {
+    const content = document.createElement('div');
+    
+    gridContainer.appendChild(content);
 
-  <link href="css/normalize.css" rel="stylesheet">
-  <link href="css/style.css" rel="stylesheet">
-</head>
-
-<body>
-
-  <p>Hello, world!</p>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-  <script src="js/script.js"></script>
-</body>
-
-</html>
+}    
