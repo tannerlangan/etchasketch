bbd779cd0a468ffee6d8a8ac5851da5410f4c65f
Tanner <t.langan.07@gmail.com>
2020-04-04 20:12:40 -0700

    added mouseover functionality to etch-a-sketch board, changes color on mousemover

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const gridContainer = document.querySelector('#gridContainer');
 
+
 for (let index = 0; index < 16*16; index++) {
     const content = document.createElement('div');
-    
     gridContainer.appendChild(content);
-
+    content.addEventListener("mouseover", () => {
+        content.setAttribute('style', 'background-color: darkkhaki;')
+    });
 }    
+
